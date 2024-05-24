00cf3a0d92996ab0faf91f11d44bd88f7f1626fd
Muhammed Shahil A K <98741470+mdshahilak@users.noreply.github.com>
2024-05-24 18:40:13 +0530

    Update urls.py
    urls updated

--- todo/urls.py
+++ todo/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('',views.home,name='home'),
     path('add_task',views.add_task,name='add_task'),
+    path('mark_as_done/<pk>',views.mark_as_done,name='mark_as_done'),
+    path('mark_as_undone/<pk>',views.mark_as_undone,name='mark_as_undone'),
+    path('update_task/<update_id>',views.update_task,name='update_task'),
 ]
