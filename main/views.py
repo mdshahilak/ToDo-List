0df2c3a609878a358bbcaa153e05f9586bdc1257
Muhammed Shahil A K <98741470+mdshahilak@users.noreply.github.com>
2024-05-24 18:37:27 +0530

    Update views.py
    views changed

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
 # Create your views here.
 def home(request):
@@ -12,6 +12,30 @@ def home(request):
 def add_task(request):
     if request.method=='POST':
         task = request.POST.get('task')
+        if not task:
+            return redirect('home')
         Todo = Task.objects.create(task=task)
         Todo.save()
         return redirect('home')
+def mark_as_done(request,pk):
+    task  = Task.objects.get(id = pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+def mark_as_undone(request, pk):
+    task  = Task.objects.get(id = pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+def update_task(request,update_id):
+    get_task=get_object_or_404(Task,id=update_id)
+    if request.method=='POST':
+        new_task=request.POST.get('new_task')
+        get_task.task=new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task,
+        }
+        return render(request,'update.html',context)
